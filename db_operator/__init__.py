6a94b730c981994fc968c83ad8289888b908d682
SDchao <wentaochens@outlook.com>
2021-05-07 21:23:53 +0800

    add inser cert

--- db_operator/__init__.py
+++ db_operator/__init__.py
@@ -10,6 +10,12 @@ cursor.execute("create table if not exists user("
                "id varchar(18) primary key, "
                "pri_key_sum varchar(32) not null,"
                "level integer default 0)")
+
+cursor.execute("create table if not exists user_hash("
+               "id varchar(18), "
+               "cert_hash varchar(32) not null, "
+               "unique (id, cert_hash))")
+
 try:
     cursor.execute("insert into user (id, pri_key_sum, level) "
                    "values ('admin', 'e10adc3949ba59abbe56e057f20f883e', '2')")
@@ -20,18 +26,39 @@ conn.commit()
 
 def insert_new_user(id, pri_key_sum):
     logger.info(f"Submitting new user to database ({id})")
-    cursor.execute("insert into user (id, pri_key_sum) "
-                   "values (?,?)",
-                   (id, pri_key_sum))
-    conn.commit()
+    try:
+        cursor.execute("insert into user (id, pri_key_sum) "
+                       "values (?,?)",
+                       (id, pri_key_sum))
+        conn.commit()
+    except sqlite3.IntegrityError:
+        logger.info(f"{id} has existed.")
+        raise UserWarning("用户已存在")
 
 
 def verify_id_key(id, pri_key_sum):
-    logger.info(f"Checking user {id} with password")
+    logger.info(f"Verifying user {id} with pri_key_sum")
     cursor.execute("select level from user where id = ? and pri_key_sum = ?", (id, pri_key_sum))
     result_tuple = cursor.fetchone()
-    logger.info(f"Result: {result_tuple}")
     if result_tuple:
+        logger.info(f"User {id} verified")
         return result_tuple[0]
     else:
+        logger.info(f"Failed to verify user {id}")
         return -1
+
+
+def insert_new_cert_hash(id, cert_hash):
+    logger.info(f"Inserting new cert_hash record {cert_hash}, belongs to {id}")
+    cursor.execute("select id from user where id = ?", (id))
+    result = cursor.fetchone()
+    if not result:
+        logger.warning(f"User {id} do not exists")
+        raise UserWarning("用户不存在")
+
+    try:
+        cursor.execute("insert into user_hash (id, cert_hash) "
+                       "VALUES (?,?)", (id, cert_hash))
+    except sqlite3.IntegrityError:
+        logger.warning(f"User {id} already have cert {cert_hash}")
+        raise UserWarning("已存在该证书")
