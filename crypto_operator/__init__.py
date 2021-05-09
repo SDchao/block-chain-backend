4399b9b15a44fde8a6fb42ebcd514ddeb936305b
SDchao <wentaochens@outlook.com>
2021-05-09 10:11:33 +0800

    modify encrypt method

--- crypto_operator/__init__.py
+++ crypto_operator/__init__.py
@@ -1,8 +1,10 @@
+import io
 import os
 
-from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Hash import MD5
 from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
 
 from custom_types import ErrorMessage
 from logger import logger
@@ -47,15 +49,34 @@ def user_encrypt(id: str, content: bytes) -> bytes:
     with open(f"user_keys/{id}_pub.pem", "rb") as f:
         key = RSA.import_key(f.read())
 
+    session_key = get_random_bytes(16)
+
     cipher_rsa = PKCS1_OAEP.new(key)
-    enc_content = cipher_rsa.encrypt(content)
-    return enc_content
+    enc_session_key = cipher_rsa.encrypt(session_key)
+
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(content)
+
+    return enc_session_key + cipher_aes.nonce + tag + ciphertext
 
 
 def user_decrypt(id: str, content: bytes) -> bytes:
     with open(f"user_keys/{id}.pem", "rb") as f:
         key = RSA.import_key(f.read())
 
+    f = io.BytesIO(content)
+    enc_session_key, nonce, tag, ciphertext = \
+        [f.read(x) for x in (key.size_in_bytes(), 16, 16, -1)]
+
     cipher_rsa = PKCS1_OAEP.new(key)
-    dec_content = cipher_rsa.decrypt(content)
+    session_key = cipher_rsa.decrypt(enc_session_key)
+
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    dec_content = cipher_aes.decrypt_and_verify(ciphertext, tag)
+
     return dec_content
+
+
+if __name__ == '__main__':
+    r = b'I LOVE YOU'
+    assert r == user_decrypt(340503, user_encrypt('340503', r))
