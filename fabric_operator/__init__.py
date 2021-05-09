9100b5c231288484f6151b6d88c4484aa6a28982
SDchao <wentaochens@outlook.com>
2021-05-09 17:19:35 +0800

    reformated

--- fabric_operator/__init__.py
+++ fabric_operator/__init__.py
@@ -1,13 +1,13 @@
-from custom_types import *
 from subprocess import getoutput
+
 from config import *
+from custom_types import *
 from logger import logger
 
 
 def put_state(key: str, value: str):
     logger.info(f"Putting state: {key} : {value}")
-    cmd = SHELL_MODIFY_PATH + \
-        "'{" + f'"Args":["Modify","{key}","{value}"]' + "}'"
+    cmd = SHELL_MODIFY_PATH + "'{" + f'"Args":["Modify","{key}","{value}"]' + "}'"
     result = getoutput(cmd)
     if result.find('Chaincode invoke successful') < 0:
         raise ErrorMessage(result)
@@ -15,8 +15,7 @@ def put_state(key: str, value: str):
 
 def get_state(key: str) -> str:
     logger.info(f"Getting state: {key}")
-    cmd = SHELL_QUERY_PATH + \
-        "'{" + f'"Args":["Query","{key}"]' + "}'"
+    cmd = SHELL_QUERY_PATH + "'{" + f'"Args":["Query","{key}"]' + "}'"
     result = getoutput(cmd)
     if result.find('Error') >= 0:
         raise ErrorMessage(result)
