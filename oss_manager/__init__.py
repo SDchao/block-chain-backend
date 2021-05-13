382ada4b5b6c3f0d9a6cd1c278a154178c9dddf0
SDchao <wentaochens@outlook.com>
2021-05-13 21:27:51 +0800

    fix incorrect cert bucket name

--- oss_manager/__init__.py
+++ oss_manager/__init__.py
@@ -1,13 +1,15 @@
 from .basic import uploadFileFromBytes, downloadFile, modifyFile
 
+CERT_BUCKET_NAME = "certs"
+
 
 def upload_cert(data: bytes) -> str:
-    return uploadFileFromBytes("CERTS", data)
+    return uploadFileFromBytes(CERT_BUCKET_NAME, data)
 
 
 def download_cert(obj_name: str) -> bytes:
-    return downloadFile("CERTS", obj_name)
+    return downloadFile(CERT_BUCKET_NAME, obj_name)
 
 
 def modify_cert(obj_name: str, data: bytes) -> str:
-    return modifyFile("CERTS", obj_name, data)
+    return modifyFile(CERT_BUCKET_NAME, obj_name, data)
