b38d2d5d96a1a6d7a27b9e8cf21a5022304d23c1
SDchao <wentaochens@outlook.com>
2021-05-13 21:41:04 +0800

    add minio logger

--- oss_manager/__init__.py
+++ oss_manager/__init__.py
@@ -1,15 +1,19 @@
+from logger import logger
 from .basic import uploadFileFromBytes, downloadFile, modifyFile
 
 CERT_BUCKET_NAME = "certs"
 
 
 def upload_cert(data: bytes) -> str:
+    logger.info(f"Uploading cert to minio")
     return uploadFileFromBytes(CERT_BUCKET_NAME, data)
 
 
 def download_cert(obj_name: str) -> bytes:
+    logger.info(f"Getting cert from minio, name: {obj_name}")
     return downloadFile(CERT_BUCKET_NAME, obj_name)
 
 
 def modify_cert(obj_name: str, data: bytes) -> str:
+    logger.info(f"Modifying cert from minio, name: {obj_name}")
     return modifyFile(CERT_BUCKET_NAME, obj_name, data)
